5db180f862ed7d583833a5aa2e5a9ef3662c04df
Amin Benselim <benselim.amin@gmail.com>
2022-12-02 00:19:00 +0100

    solve part 1 in go

--- day1/part1.go
+++ day1/part1.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"fmt"
-
 	"amin/adv2022/helperUtils"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	// Call the function and print the result
 	lines := helperUtils.ReadFileLines("./part1.txt")
+	var maxCalories int64 = 0
+	var currentCalories int64 = 0
 	for _, line := range lines {
-		fmt.Println(line)
+		if line == "" {
+			currentCalories = 0
+		} else {
+			// Convert the string to an int value, first trim the new line character
+			n, err := strconv.ParseInt(strings.TrimRight(line, "\r\n"), 10, 0)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			currentCalories += n
+
+			if currentCalories > maxCalories {
+				maxCalories = currentCalories
+			}
+		}
 	}
+
+	fmt.Println(maxCalories)
 }
